d8006a04a350a10925ce17307f45ff030dae10df
Andrey Mudri <andreybeckert@gmail.com>
2023-08-04 10:25:50 -0300

    patch usage

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get, Injectable, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { editUserDto } from './dto';
+import { UserService } from './user.service';
 
-@Injectable()
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private UserService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
   }
+
   @Patch()
-  editUser() {
-    return 'ok';
+  update(@GetUser('id') userId: number, @Body() dto: editUserDto) {
+    return this.UserService.editUser(userId, dto);
   }
 }
